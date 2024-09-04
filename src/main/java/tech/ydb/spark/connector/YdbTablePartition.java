3dcda8e9718d3327e81f87e9ef749d0839896214
Maksim Zinal <zinal@ydb.tech>
2024-09-04 10:35:22 +0300

    randomize order of partitions for better scan parallelism

--- src/main/java/tech/ydb/spark/connector/YdbTablePartition.java
+++ src/main/java/tech/ydb/spark/connector/YdbTablePartition.java
@@ -11,12 +11,18 @@ import org.apache.spark.sql.connector.read.InputPartition;
  */
 public class YdbTablePartition implements InputPartition {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
+    private final int orderingKey;
     private final YdbKeyRange range;
 
-    public YdbTablePartition(YdbKeyRange range) {
+    public YdbTablePartition(int orderingKey, YdbKeyRange range) {
+        this.orderingKey = orderingKey;
         this.range = range;
+    }
+
+    public int getOrderingKey() {
+        return orderingKey;
     }
 
     public YdbKeyRange getRange() {
