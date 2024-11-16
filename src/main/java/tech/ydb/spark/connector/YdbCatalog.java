907a16a6c6d8cbfcf9bb3342dbf668d9b50cf130
Maksim Zinal <zinal@ydb.tech>
2024-11-16 19:52:17 +0300

    new snapshot version 1.4

--- src/main/java/tech/ydb/spark/connector/YdbCatalog.java
+++ src/main/java/tech/ydb/spark/connector/YdbCatalog.java
@@ -52,7 +52,7 @@ public class YdbCatalog extends YdbOptions
             = org.slf4j.LoggerFactory.getLogger(YdbCatalog.class);
 
     // X.Y[-SNAPSHOT]
-    public static final String VERSION = "1.3-SNAPSHOT";
+    public static final String VERSION = "1.4-SNAPSHOT";
     public static final String INDEX_PREFIX = "ix/";
     public static final String ENTRY_TYPE = "ydb_entry_type";
     public static final String ENTRY_OWNER = "ydb_entry_owner";
